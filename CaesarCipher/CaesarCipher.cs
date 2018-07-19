ae751961d20f0a6486165ee908d83ec0d1075152
arjun <arjunporwal10@gmail.com>
2018-07-19 11:27:22 +0530

    all test passed

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
 
 namespace CaesarCipher
 {
@@ -7,7 +8,35 @@ namespace CaesarCipher
     {
         public static string Rotate(string text, int shiftKey)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            //throw new NotImplementedException("You need to implement this function.");
+
+            string message = "";
+            foreach(char c in text)
+            {
+                if (((int)c >= (int)'a' && (int)c <= (int)'z'))
+                {
+
+                    int asciiValueOfCharacter = (int)c;
+                    asciiValueOfCharacter += shiftKey;
+                    if (asciiValueOfCharacter > (int)'z')
+                        asciiValueOfCharacter -= 26;
+                    char intergerToCharacter = (char)asciiValueOfCharacter;
+                    message += intergerToCharacter;
+                }
+                else if (((int)c >= (int)'A' && (int)c <= (int)'Z'))
+                {
+
+                    int asciiValueOfCharacter = (int)c;
+                    asciiValueOfCharacter += shiftKey;
+                    if (asciiValueOfCharacter > (int)'Z')
+                        asciiValueOfCharacter -= 26;
+                    char intergerToCharacter = (char)asciiValueOfCharacter;
+                    message += intergerToCharacter;
+                }
+                else message += c;
+            }
+            return message;
+
         }
     }
 }
